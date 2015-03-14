4813bdf3d35b604582aecee09256cc79abf5234d
Huanhuan Wang <whh.reg@gmail.com>
2015-03-14 22:14:03 +0800

    sf

--- cloud/main.js
+++ cloud/main.js
@@ -3,3 +3,17 @@
 AV.Cloud.define("hello", function(request, response) {
   response.success("Hello world!");
 });
+
+
+AV.Cloud.define('user', function(request, response) {
+  var query = new AV.Query('SuggestionByUser');
+  query.equalTo('UserObjectId', request.params.userid);
+  query.find({
+    success: function(results) {
+      response.success(results);
+    },
+    error: function() {
+      response.error('lookup failed');
+    }
+  });
+});
